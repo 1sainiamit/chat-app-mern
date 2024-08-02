9232763041b0beb3144fc905fd1e3a0e8c832f18
Amit Saini <amitsaini2001.18@gmail.com>
2024-08-02 09:19:43 +0530

    added protectroute, user, message, and conversation model

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,33 @@
+import Conversation from "../models/Conversation.model.js";
+import Message from "../models/Message.model.js";
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id: recieverId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, recieverId] },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, recieverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      recieverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage)._id;
+    }
+    res.status(201).json({ message: "Message sent" });
+  } catch (error) {
+    console.log("Error in message controller: ", error.message);
+    res.status(500).json({ error: "Internl Server error" });
+  }
+};
